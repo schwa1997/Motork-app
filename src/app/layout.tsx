dcddd7179197cb3bf0452bc5bbed46b549b36b8c
huimin chen <huimin.chen@studenti.unipd.it>
2023-10-04 14:14:13 +0200

    updated logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Footer } from "./components/Footor/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Motork",
+  title: "Demo",
   description: "Designed by Huimin Chen",
 };
 
